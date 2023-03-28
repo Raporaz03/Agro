ec9e5510083263a734ee4882c3ecfab89063d6b3
Raja Singh <rajasgh18@gmail.com>
2023-03-28 21:27:13 +0530

    Crops and finance done

---
+++ src/dummyData.js
@@ -0,0 +1,39 @@
+export const finance = [
+    {
+        _id:1,
+        title: "Government Schemes",
+        url: "https://st.adda247.com/https://wpassets.adda247.com/wp-content/uploads/multisite/sites/5/2022/05/13172737/All-schemes-of-government-of-india.png",
+        text: "Some quick example text to build on the card title and make up the bulk of the card's content."
+    },
+    {
+        _id:2,
+        title:"NGO",
+        url:"https://i0.wp.com/whatiscomputer.in/wp-content/uploads/2020/06/NGO-1.png?fit=1280%2C720&ssl=1", 
+        text: "Some quick example text to build on the card title and make up the bulk of the card's content."
+    },
+    {
+        _id:3,
+        title:"Donation",
+        url:"https://c.ndtvimg.com/2022-11/qovd224_image_625x300_11_November_22.jpg?ver-20230302.111", 
+        text:"Some quick example text to build on the card title and make up the bulk of the card's content."
+    },
+    {
+        _id:4,
+        title:"Self Help Group",
+        url:"https://images.livemint.com/img/2022/03/07/1600x900/oped1_1646670646465_1646670652294.jpg", 
+        text:"Some quick example text to build on the card title and make up the bulk of the card's content."
+    },
+]
+
+export const crops = [
+    {
+        name: "Wheat",
+        url: "https://www.agric.wa.gov.au/sites/gateway/files/AdobeStock_113261787.jpeg",
+        text:"Some quick example text to build on the card title and make up the bulk of the card's content."
+    },
+    {
+        name: "Rice",
+        url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRY_38OB-O3ct4_WTA4CLOW7rpDmuU8RkDVsQ&usqp=CAU",
+        text:"Some quick example text to build on the card title and make up the bulk of the card's content."
+    }
+]
