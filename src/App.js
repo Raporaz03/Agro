e3a49251961a87393302f96b210fb800b18fa6d8
Raja Singh <rajasgh18@gmail.com>
2023-03-28 17:01:27 +0530

    changes to sidebar and navbar

--- src/App.js
+++ src/App.js
@@ -19,20 +19,22 @@ function App() {
   return (
     <Router>
       <div className='App'>
-        <Sidebar />
+        <Navbar />
         <div className='mainApp'>
-          <Navbar />
-          <Routes>
-            <Route exact path="/" element={<Home />} />
-            <Route exact path="/profile" element={<Profile />} />
-            <Route exact path="/dashboard" element={<Dashboard />} />
-            <Route exact path="/equipment" element={<Equipment />} />
-            <Route exact path="/workforce" element={<Workforce />} />
-            <Route exact path="/specialists" element={<Specialists />} />
-            <Route exact path="/stores" element={<Stores />} />
-            <Route exact path="/finance" element={<Finance />} />
-            <Route exact path="/majorcrops" element={<Majorcrops />} />
-          </Routes>
+          <Sidebar />
+          <div className='mainAppPage'>
+            <Routes>
+              <Route exact path="/" element={<Home />} />
+              <Route exact path="/profile" element={<Profile />} />
+              <Route exact path="/dashboard" element={<Dashboard />} />
+              <Route exact path="/equipment" element={<Equipment />} />
+              <Route exact path="/workforce" element={<Workforce />} />
+              <Route exact path="/specialists" element={<Specialists />} />
+              <Route exact path="/stores" element={<Stores />} />
+              <Route exact path="/finance" element={<Finance />} />
+              <Route exact path="/majorcrops" element={<Majorcrops />} />
+            </Routes>
+          </div>
         </div>
       </div>
     </Router>
--- src/Pages/home/Home.jsx
+++ src/Pages/home/Home.jsx
@@ -2,8 +2,28 @@ import "./home.css";
 
 export default function Home() {
   return (
-    <>
-      This is Home
-    </>
+    <div classNameName="homeWrapper">
+      <div id="carouselExample" className="carousel slide">
+        <div className="carousel-inner">
+          <div className="carousel-item active">
+            <img src="..." className="d-block w-100" alt="..." />
+          </div>
+          <div className="carousel-item">
+            <img src="..." className="d-block w-100" alt="..." />
+          </div>
+          <div className="carousel-item">
+            <img src="..." className="d-block w-100" alt="..." />
+          </div>
+        </div>
+        <button className="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span className="visually-hidden">Previous</span>
+        </button>
+        <button className="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+          <span className="carousel-control-next-icon" aria-hidden="true"></span>
+          <span className="visually-hidden">Next</span>
+        </button>
+      </div>
+    </div>
   )
 }
