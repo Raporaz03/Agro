6a3a854485911d91f35e8ada1d172f291d7d93d2
Raja Singh <rajasgh18@gmail.com>
2023-03-29 00:04:11 +0530

    changes to newsItem

--- src/Pages/news/News Item.js
+++ src/Pages/news/News Item.js
@@ -1,19 +1,19 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
-export default class NewsItem extends Component{
-    render(){
-        let {title, description, imageUrl, newsUrl, author, date, source} = this.props;
-        return(
+export default class NewsItem extends Component {
+    render() {
+        let { title, description, imageUrl, newsUrl, author, date, source } = this.props;
+        return (
             <>
-                <div className="card" style={{width: "18rem"}}>
-                    <div style={{display:"flex",position:"absolute", right:"0"}}>
-                        <span className="badge rounded-pill bg-danger">{source}</span>
+                <div className="card" style={{ width: "18rem" }}>
+                    <div style={{ display: "flex", position: "absolute", bottom: "20px", right: "20px" }}>
+                        <span className="badge rounded bg-success">{source}</span>
                     </div>
-                <img src={!imageUrl? "https://assets.telecomtv.com/assets/telecomtv/apple-sos-15523.jpeg?w=1200":imageUrl} className="card-img-top" alt="..."/>
+                    <img src={!imageUrl ? "https://assets.telecomtv.com/assets/telecomtv/apple-sos-15523.jpeg?w=1200" : imageUrl} className="card-img-top" alt="..." />
                     <div className="card-body">
                         <h5 className="card-title">{title}...</h5>
                         <p className="card-text">{description}...</p>
-                        <p className="card-text text-muted">By {author?author:"Unknown"} on {new Date(date).toGMTString()}</p>
+                        <p className="card-text text-muted">By {author ? author : "Unknown"} on {new Date(date).toGMTString()}</p>
                         <a href={newsUrl} className="btn btn-sm btn-primary">Read More</a>
                     </div>
                 </div>
