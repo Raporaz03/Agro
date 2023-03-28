32c296def29de1a25318325d5e2200e68ece8926
Shreya Badgaiyan <shreyabadgaiyan14@gmail.com>
2023-03-28 23:36:59 +0530

    About page done:)

--- src/Pages/about/About.jsx
+++ src/Pages/about/About.jsx
@@ -2,7 +2,56 @@ import "./about.css";
 
 export default function About() {
   return (
-    <>
-    </>
+    <div className="aboutWrapperIntro">
+      <h2><strong>ABOUT US</strong></h2>
+      <div className="aboutWrapperIntro">
+        <h4>Inroduction:</h4>
+        <p> Farming and agriculture have always been one of the essential economic activities in India.<br/>
+           Near about two-thirds of the population engaged in agriculture and agriculture-related activities,<br/>
+           it is crucial to understand India’s plight and why they are struggling. For a few decades, the working<br/>
+          environment and the agricultural scenario are stagnant, and it is no more a profit-making sector like before.<br/>
+          Our aim is to empower the agricultural sector in India.<br/>
+          There is high need that farmers get awared about the new technologies and equiments that can be very benificial<br/>
+         for increase of yield.
+
+        </p>
+        <img className="aboutWrapperInfoImg"
+        src={"https://themanilajournal.com/wp-content/uploads/2021/07/Helping-Farmers-in-the-Philippines-with-Accessible-Financial-Help.jpeg"}
+        alt="farmer"/>
+        <h3>Some General Issues in the Agricultural Sector</h3>
+        <ul>
+          <li>
+          Small and fragmented landholdings: One of the primary reasons why Indian farmers are not competent is because of small <br/>andfragmented holdings. Most of the poor farmers have a minimal amount of land and too fragmented. According to data, the average<br/>size of landholding per farmers is decreasing every year. That turns to be the worst. This problem becomes more severe<br/> in highly populated states with less farming land. As per the data, only 1.6% of total land holdings belong to large <br/>holding that too by the wealthy ones. It becomes impossible for a farmer to irrigate and harvest in fragmented lands because<br/> a lot of time gets wasted in moving the resources from one place to another. 
+          </li>
+          <li>
+          The issue in Dealing with local traders or any middleman: It is a significant problem a farmer faces at the end of all <br/>agricultural activity.  Due to the absence of proper mandi houses and appropriate marketplaces, farmers fail to reasonably sell their crops. In this situation, they have to look for a middleman and local traders for business.
+          </li>
+          <li>
+          Seed related issues for farming: The seed is a critical and necessary input for obtaining higher and better crop yields and <br/> sustained agricultural production growth. Distribution of assured quality seed is as essential as the production of such seeds. <br/> Unluckily, good quality seeds are out of reach for most farmers, mostly small and marginal farmers, mainly because of better<br/>  seeds’ exorbitant prices
+          </li>
+          <li>Manures, Fertilizers, and Biocides: The farmers have used Indian soils for growing crops over thousands of years without <br/> caring much for its replenishing character, which has led to depletion and exhaustion of soils resulting in the low productivity <br/> of crops. The average yield of almost all the produce is the lowest globally and is a severe problem that can be solved <br/> using more manures and fertilizers.</li>
+          <li>Lack of proper mechanization: Despite the large scale mechanization of agriculture in some parts of the country, most larger <br/> agricultural sectors are carried on manually by human hand using conventional and straightforward tools and implements like <br/> a wooden plough, sickle, etc.</li>
+          <li>Lack of adequate tools and equipment: Even in this era, most Indian farmers use traditional agriculture tools, such as plough,<br/>  sickle, etc. That leads to the wastage of energy and workforce and less yield per capita labor force. Only little use of<br/>  the machine is seen in Irrigation, harvesting, and transportation. There are two reasons why farmers are not using modern <br/> equipment. First, they are not aware, and second, they can’t afford such equipment.</li>
+          <li>
+          Scarcity of capital: Agriculture is the most crucial industry in India, and like all other industries, it also requires capital.<br/>  The role of capital input is becoming important with every passing day due to farm technology advancement. Since the <br/> agriculturists’ capital is all locked up in his lands, he is obligated to borrow money for stimulating the agricultural produce vfor daily living. 
+          </li>
+        </ul>
+        <h3>OUR OBJECTIVE</h3>
+        <p>Our objective is to help the farmers in the best way we can. Through our portal we will be educating the farmers about the new technologies in the field of agriculture which will surely help them get better help , better income and hence better livelihood</p>
+        <h4>What this website provides to the farmers?</h4>
+        <ul>
+          <l>Educates farmer about the new technologies and equipments in agriculture.</l>
+          <li>Connects the wiling workforce and labourers to zamindaars and landlords to work at their fields</li>
+          <li>Connects the farmers to the stores which can give them resources such as tractors and harvestors at nominal rates or at rent.</li>
+          <li>Awares the farmers about different policies and schemes which could be beneficial for the farmers.</li>
+          <li>Gives the knowledge about the best practices to grow important crops.</li>
+          <li>Connects the farmers to specialists of different domains of agriculture to ask out their queries.</li>
+          <li>Gives idea about the different resources which can help them to gain finance for agriculture such as NGOs, Self Help Groups,etc.</li>
+          <li>Gives Tthe latest news related to farming and agriculture.</li>
+          </ul>
+
+
+      </div>
+    </div>
   );
 }
